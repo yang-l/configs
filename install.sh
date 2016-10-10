d4e17a29dff4e575c772acfb6df8898df9eff124
YL <Y@L>
2016-10-10 15:30:41 +1300

    added aws cli config template

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-for i in .config/awesome
+for i in .config/awesome .aws
 do
     [ -d ~/"${i}" ] && rm -fr ~/"${i}"
     mkdir -p ~/"${i}"
 done
 
-for i in .bash_aliases .bash_profile .tmux.conf .Xdefaults .xsessionrc .config/awesome/rc.lua .config/bashrc
+for i in .bash_aliases .bash_profile .tmux.conf .Xdefaults .xsessionrc .config/awesome/rc.lua .config/bashrc .aws/config
 do
     [ -e ~/"${i}" ] && rm -f ~/"${i}"
     ln -s $(pwd)/"${i}" ~/"${i}"
