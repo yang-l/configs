fbf2be106c8451dca941579b09b7b01f70b81fc2
YL <Y@L>
2017-02-17 12:35:36 +1300

    updated keychain for osx

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,6 @@
 # This file is called by login shell, whereas .bashrc is called by interactive non-login shell
 if [ -f ~/.bashrc ]; then
     source ~/.bashrc
+else
+    source ~/.config/bashrc
 fi
--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p .config/hist_backup
+mkdir -p ~/.config/hist_backup
 
-for i in .config/awesome .aws
+for i in .config/awesome .config/keychain .keychain .aws
 do
     [ -d ~/"${i}" ] && rm -fr ~/"${i}"
     mkdir -p ~/"${i}"
 done
 
-for i in .bash_aliases .bash_profile .tmux.conf .Xdefaults .xsessionrc .config/awesome/rc.lua .config/bashrc .aws/config
+for i in .bash_aliases .bash_profile .tmux.conf .Xdefaults .xsessionrc .config/awesome/rc.lua .config/bashrc .config/keychain/keychain .aws/config
 do
     [ -e ~/"${i}" ] && rm -f ~/"${i}"
     ln -s $(pwd)/"${i}" ~/"${i}"
