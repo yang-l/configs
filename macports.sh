6beccd52ddde98be66b785e722f4cd192dc979f7
Y@L <Y@L>
2019-08-02 16:45:10 +1200

    config update

--- macports.sh
+++ macports.sh
@@ -10,11 +10,11 @@ port -t install \
      emacs-app `#text editor` \
      mas `# mac app store cli` \
      p7zip `# archive tool` \
-     `# rbenv ruby-build` `# ruby` \
+     rbenv ruby-build `# ruby` \
      the_silver_searcher `# code searching tool` \
      tmux `# terminal multiplexer` \
      socat xorg-server `# docker/x11/gui` \
-     python37 py37-virtualenv py27-virtualenv
+     python37 py37-virtualenv py27-virtualenv `# python`
 
 # others to install
-## xcode / iterm2 / docker / virtualbox & vagrant & packer / Terminus TTF
+## xcode / iterm2 / docker / virtualbox & vagrant & packer / Terminus TTF / pwsh
