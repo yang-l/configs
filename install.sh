600c5c9c6515919166a8d532900fa885cfa3ea13
Y@L <Y@L>
2019-08-15 17:03:11 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 mkdir -p ~/.config/hist_backup
 
 for i in .keychain
@@ -11,16 +13,19 @@ done
 # link file
 for i in .bash_aliases .bash_profile .inputrc .tmux.conf .Xdefaults .xsessionrc .config/bashrc
 do
-    [ -e ~/"${i}" ] && rm -f ~/"${i}"
+    [ -L ~/"${i}" ] && rm -f ~/"${i}"
     ln -s $(pwd)/"${i}" ~/"${i}"
 done
 
 # link folder
 for i in .aws .config/awesome .config/iterm2 .config/keychain .config/vagrant
 do
-    [ -e ~/"${i}" ] && rm -f ~/"${i}"
+    [ -L ~/"${i}" ] && rm -f ~/"${i}"
     ln -s $(pwd)/"${i}"/ ~/"${i}"
 done
 
 # install git-completion.bash
 curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+# terminal truecolor
+$( cd "$( dirname "$0" )" && pwd )/.config/terminfo.sh
