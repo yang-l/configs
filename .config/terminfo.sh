b4999d057955b2aa1a104fde90eac4d5ad128883
Y@L <Y@L>
2022-01-17 14:22:54 +1300

    update terminfo colour info script

--- .config/terminfo.sh
+++ .config/terminfo.sh
@@ -7,10 +7,12 @@ infocmp -x xterm-256color > ${TMP_MI}
 
 printf "\tTc,\n" >> ${TMP_MI}
 
-# This one is for iTerm2 under macOS, may require changes under Linux
-# only work with macports emacs-app "sudo port install emacs-app"
-printf '\t%s\n' "setb24=\E[48:2:%p1%{65536}%/%d:%p1%{256}%/%{255}%&%d:%p1%{255}%&%dm," >> ${TMP_MI}
-printf '\t%s\n' "setf24=\E[38:2:%p1%{65536}%/%d:%p1%{256}%/%{255}%&%d:%p1%{255}%&%dm," >> ${TMP_MI}
+# https://www.gnu.org/software/emacs/manual/html_node/efaq/Colors-on-a-TTY.html
+printf '\t%s\n' "setb24=\E[48\:2\:\:%p1%{65536}%/%d\:%p1%{256}%/%{255}%&%d\:%p1%{255}%&%dm," >> ${TMP_MI}
+printf '\t%s\n' "setf24=\E[38\:2\:\:%p1%{65536}%/%d\:%p1%{256}%/%{255}%&%d\:%p1%{255}%&%dm," >> ${TMP_MI}
+
+printf '\t%s\n' "setab=\E[%?%p1%{8}%<%t4%p1%d%e48\:2\:\:%p1%{65536}%/%d\:%p1%{256}%/%{255}%&%d\:%p1%{255}%&%d%;m," >> ${TMP_MI}
+printf '\t%s\n' "setaf=\E[%?%p1%{8}%<%t3%p1%d%e38\:2\:\:%p1%{65536}%/%d\:%p1%{256}%/%{255}%&%d\:%p1%{255}%&%d%;m," >> ${TMP_MI}
 
 tic -x ${TMP_MI}
 
