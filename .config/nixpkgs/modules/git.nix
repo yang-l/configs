c57301a11f767c87177aafb59b284fa5ce169d57
Y@L <Y@L>
2022-02-26 21:23:51 +1300

    Add global git pre-push hook to prevent to push to protected branch directly

--- .config/nixpkgs/modules/git.nix
+++ .config/nixpkgs/modules/git.nix
@@ -6,7 +6,14 @@
     delta
   ];
 
-  home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables._BASE_CONFIG_FOLDER_PATH}/.gitconfig";
+  home.file = {
+    ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables._BASE_CONFIG_FOLDER_PATH}/.gitconfig";
+    ".git-templates/hooks" = {
+      source = ../../../.git-templates/hooks;
+      recursive = true;
+    };
+  };
+
   home.activation.bash-git-completion = lib.hm.dag.entryAfter ["writeBoundary"] ''
     $DRY_RUN_CMD bash -c 'set -x ; curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash'
   '';
