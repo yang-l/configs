aa0262fcb1ad93829583f588a5cdefbea0d1c50d
Y@L <Y@L>
2022-04-19 17:13:17 +1200

    Fixup for tmux-network-bandwidth plugin

--- .config/nixpkgs/modules/tmux.nix
+++ .config/nixpkgs/modules/tmux.nix
@@ -4,6 +4,9 @@
   home.packages = with pkgs; [
     tmux
     tmux-mem-cpu-load
+    # for tmux-network-bandwidth plugin
+    coreutils
+    gawkInteractive
   ];
 
   home.file.".tmux.conf".source = ../../../.tmux.conf;
