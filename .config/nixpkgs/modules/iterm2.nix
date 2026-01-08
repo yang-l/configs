9c6c24d5a6a33dee52d647bb58c56b2d1f7b1932
Y@L <Y@L>
2026-01-08 13:33:59 +1300

    Version bump

--- .config/nixpkgs/modules/iterm2.nix
+++ .config/nixpkgs/modules/iterm2.nix
@@ -4,7 +4,6 @@
   home.packages = with pkgs; [
     iterm2
     maple-mono.NormalNL-CN-unhinted
-    terminus_font_ttf
   ];
 
   xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables._BASE_CONFIG_FOLDER_PATH}/.config/iterm2/com.googlecode.iterm2.plist";
